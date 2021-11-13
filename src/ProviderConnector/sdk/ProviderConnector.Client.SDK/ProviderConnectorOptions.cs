ad030f636373e13d067a0b6a45a2e2b0b4e8f124
Revazashvili <levanrevazashvili@gmail.com>
2021-11-13 22:16:11 +0400

    add sdk project

---
+++ src/ProviderConnector/sdk/ProviderConnector.Client.SDK/ProviderConnectorOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace ProviderConnector.Client.SDK;
+
+public class ProviderConnectorOptions
+{
+    public string Url { get; set; } = null!;
+}
---
+++ src/ProviderConnector/sdk/ProviderConnector.Client.SDK/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ProviderConnector.Client.SDK;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddProviderConnector(this IServiceCollection services,Action<ProviderConnectorOptions> action)
+    {
+        var options = GetOptions(action);
+        
+        // inject clients here
+        
+        return services;
+    }
+
+    public static ProviderConnectorOptions GetOptions(Action<ProviderConnectorOptions> action)
+    {
+        var options = new ProviderConnectorOptions();
+        action.Invoke(options);
+        return options;
+    }
+}
