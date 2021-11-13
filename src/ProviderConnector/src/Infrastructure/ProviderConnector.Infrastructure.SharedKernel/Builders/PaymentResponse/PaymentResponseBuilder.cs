27498d1a2a2b4aba9050212500a3554884373093
Revazashvili <levanrevazashvili@gmail.com>
2021-11-13 18:28:03 +0400

    add PaymentResponseBuilder.cs

---
+++ src/ProviderConnector/src/Infrastructure/ProviderConnector.Infrastructure.SharedKernel/Builders/PaymentResponse/PaymentResponseBuilder.cs
@@ -0,0 +1,33 @@
+﻿using Forbids;
+using ProviderConnector.Core.Models.Enums;
+
+namespace ProviderConnector.Infrastructure.SharedKernel.Builders.PaymentResponse;
+
+public class PaymentResponseBuilder : ITransactionIdStage,IStatusStage,IPaymentBuilder
+{
+    private Core.Models.Responses.PaymentResponse _paymentResponse;
+    private PaymentResponseBuilder() => _paymentResponse = new();
+
+    public static ITransactionIdStage Create() => new PaymentResponseBuilder();
+
+    public IStatusStage WithTransactionId(long transactionId)
+    {
+        _paymentResponse.TransactionId = Forbid.From.Zero(transactionId);
+        return this;
+    }
+    
+    public IPaymentBuilder WithStatus(PaymentStatus status)
+    {
+        _paymentResponse.Status = status;
+        return this;
+    }
+    
+    public PaymentResponseBuilder Failed()
+    {
+        _paymentResponse.Status = PaymentStatus.UndefinedError;
+        _paymentResponse.TransactionId = 0;
+        return this;
+    }
+    
+    public Core.Models.Responses.PaymentResponse Build() => _paymentResponse;
+}
--- src/ProviderConnector/src/Infrastructure/ProviderConnector.Infrastructure.SharedKernel/Builders/ResponseBuilder/Build.cs
+++
@@ -1,9 +0,0 @@
-﻿using ProviderConnector.Infrastructure.SharedKernel.Builders.ResponseBuilder.Responses;
-
-namespace ProviderConnector.Infrastructure.SharedKernel.Builders.ResponseBuilder;
-
-public class Build
-{
-    public static GetBalanceResponseBuilder Balance { get; } = new();
-    public static PaymentResponseBuilder Payment { get; } = new();
-}
