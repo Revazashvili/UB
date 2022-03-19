c7fde608f95a0a6bd65c2185be0938c0f0d8d70f
Revazashvili <levanrevazashvili@gmail.com>
2022-03-19 20:34:41 +0400

    refactor(SharedKernel): convert GetAllTypeWithProviderAttribute to expression body with linq

--- src/ProviderConnector/src/Infrastructure/ProviderConnector.Infrastructure.SharedKernel/Brokers/TypeBroker.cs
+++ src/ProviderConnector/src/Infrastructure/ProviderConnector.Infrastructure.SharedKernel/Brokers/TypeBroker.cs
@@ -3,27 +3,15 @@ using ProviderConnector.Core.Providers.Attributes;
 
 namespace ProviderConnector.Infrastructure.SharedKernel.Brokers;
 
-/// <summary>
-///     Broker interface for types.
-/// </summary>
 public interface ITypeProvider
 {
-    /// <summary>
-    ///     Return all type with <see cref="ProviderAttribute" /> in executing assembly.
-    /// </summary>
-    /// <returns>
-    ///     <see cref="IEnumerable{T}" />
-    /// </returns>
     IEnumerable<Type> GetAllTypeWithProviderAttribute();
 }
 
 public class TypeProvider : ITypeProvider
 {
-    public IEnumerable<Type> GetAllTypeWithProviderAttribute()
-    {
-        var assembly = Assembly.GetExecutingAssembly();
-        foreach (var type in assembly.GetTypes())
-            if (type.GetCustomAttributes(typeof(ProviderAttribute), true).Length > 0)
-                yield return type;
-    }
+    public IEnumerable<Type> GetAllTypeWithProviderAttribute() =>
+        Assembly.GetExecutingAssembly().GetTypes()
+            .Where(type => type.GetCustomAttributes(typeof(ProviderAttribute), true).Length > 0)
+            .AsEnumerable();
 }
