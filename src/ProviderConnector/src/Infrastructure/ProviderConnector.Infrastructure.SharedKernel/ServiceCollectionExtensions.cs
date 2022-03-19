d7282a28b7fe83feccfe5f1832ad89f83fb2ac69
Revazashvili <levanrevazashvili@gmail.com>
2022-03-19 20:41:14 +0400

    style(SharedKernel): remove comments from AddProviderFactory

--- src/ProviderConnector/src/Infrastructure/ProviderConnector.Infrastructure.SharedKernel/ServiceCollectionExtensions.cs
+++ src/ProviderConnector/src/Infrastructure/ProviderConnector.Infrastructure.SharedKernel/ServiceCollectionExtensions.cs
@@ -4,14 +4,8 @@ using ProviderConnector.Infrastructure.SharedKernel.Factories;
 
 namespace ProviderConnector.Infrastructure.SharedKernel;
 
-/// <summary>
-///     Extension class for <see cref="IServiceCollection" /> interface.
-/// </summary>
 public static class ServiceCollectionExtensions
 {
-    /// <summary>
-    ///     Adds <see cref="IProviderFactory" /> interface into DI Container.
-    /// </summary>
     public static IServiceCollection AddProviderFactory(this IServiceCollection services)
     {
         services.AddScoped<ITypeProvider, TypeProvider>();
